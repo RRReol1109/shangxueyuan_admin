c7c04d9580f4177b95e73b6eb7b8b25560d73064
ljf <1679578082@qq.com>
2020-07-05 19:50:14 +0800

    workload updata

--- pages/workload/reportForm.vue
+++ pages/workload/reportForm.vue
@@ -55,21 +55,45 @@
       ></el-table-column>
       <el-table-column :show-overflow-tooltip="true" prop="year" align="center" label="年份"></el-table-column>
       <el-table-column :show-overflow-tooltip="true" prop="userName" align="center" label="教师姓名"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="指导论文" align="center" label="论文指导"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="指导研究生" align="center" label="研究生指导"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="课堂教学" align="center" label="课堂教学"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="实习" align="center" label="实习"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="沙盘模拟" align="center" label="沙盘模拟"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="社会调查" align="center" label="社会调查"></el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="指导论文" align="center" label="论文指导">
+        <template slot-scope="scope">
+          <span>{{scope.row.指导论文 | statusFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="指导研究生" align="center" label="研究生指导">
+        <template slot-scope="scope">
+          <span>{{scope.row.指导研究生 | statusFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="课堂教学" align="center" label="课堂教学">
+        <template slot-scope="scope">
+          <span>{{scope.row.课堂教学 | statusFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="实习" align="center" label="实习">
+        <template slot-scope="scope">
+          <span>{{scope.row.实习 | statusFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="沙盘模拟" align="center" label="沙盘模拟">
+        <template slot-scope="scope">
+          <span>{{scope.row.沙盘模拟 | statusFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :show-overflow-tooltip="true" prop="社会调查" align="center" label="社会调查">
+        <template slot-scope="scope">
+          <span>{{scope.row.社会调查 | statusFilter}}</span>
+        </template>
+      </el-table-column>
       <el-table-column :show-overflow-tooltip="true" prop="scores" align="center" label="合计"></el-table-column>
-      <el-table-column :show-overflow-tooltip="true" prop="scores" align="center" label="个人数据">
+      <el-table-column :show-overflow-tooltip="true" align="center" label="个人数据">
         <template slot-scope="scope">
           <el-button @click="operate='show';showDialog(scope.row)" type="text" size="normal">查看</el-button>
         </template>
       </el-table-column>
     </el-table>
     <el-drawer size="60%" style="min-height:500px" title="详情" :visible.sync="dialogDetailVisible">
-      <Highcharts id="teacherStatistic" :option="option" />
+      <!-- <Highcharts id="teacherStatistic" :option="option" /> -->
       <Highcharts id="departmentStatistic" :option="statisticOption" />
     </el-drawer>
   </div>
@@ -179,13 +203,13 @@ export default {
         },
         xAxis: {
           categories: [
-            "管理科学与信息管理系",
-            "企业管理系",
-            "金融学系",
-            "财务与投资管理系",
-            "经济与贸易系",
-            "会计学系",
-            "市场营销系"
+            // "管理科学与信息管理系",
+            // "企业管理系",
+            // "金融学系",
+            // "财务与投资管理系",
+            // "经济与贸易系",
+            // "会计学系",
+            // "市场营销系"
           ],
           gridLineWidth: 2,
           min: 0,
@@ -210,32 +234,7 @@ export default {
             pointWidth: 10 //柱子宽bai度du
           }
         },
-        series: [
-          {
-            name: "论文指导",
-            data: [107, 131, 135, 203, 152, 114, 154]
-          },
-          {
-            name: "研究生指导",
-            data: [133, 156, 147, 208, 196, 114, 154]
-          },
-          {
-            name: "课堂教学",
-            data: [173, 114, 154, 132, 134, 114, 154]
-          },
-          {
-            name: "实习",
-            data: [173, 114, 154, 132, 134, 114, 154]
-          },
-          {
-            name: "沙盘模拟",
-            data: [173, 114, 154, 132, 134, 114, 154]
-          },
-          {
-            name: "社会调查",
-            data: [173, 114, 154, 132, 134, 114, 154]
-          }
-        ]
+        series: []
       },
       query: {
         limit: 10,
@@ -249,9 +248,11 @@ export default {
   },
   filters: {
     statusFilter: function(value) {
-      return {
-        "": 0
-      }[value.toString()];
+      if (value == undefined) {
+        return 0;
+      } else {
+        return value;
+      }
     }
   },
   async mounted() {
@@ -266,16 +267,43 @@ export default {
   methods: {
     async show() {
       this.dialogDetailVisible = true;
-      let res = await axios.$get("workload/charts/department", {
-        param: this.query
-      });
-      console.log(JSON.parse(res));
-      console.log(res + "======res");
-      console.log(res.data + "======data");
-      for (const key in res) {
-        if (res.hasOwnProperty(key)) {
-          const element = res[key];
-          console.log(element + "======element");
+      let param = {
+        year: this.query.year,
+        department: this.query.department
+      };
+      let res = await axios.$get("workload/charts/department", param);
+      let items = [];
+      this.statisticOption.series = [];
+      this.statisticOption.xAxis.categories = [];
+      for (const key in res.data) {
+        if (res.data.hasOwnProperty(key)) {
+          const element = res.data[key];
+          this.statisticOption.xAxis.categories.push(key);
+          items.push(element);
+        }
+      }
+      console.log(items);
+      let names = {
+        internshipScore: "实习",
+        paperScore: "论文指导",
+        postgraduateScore: "研究生指导",
+        surveyScore: "社会调查",
+        simulationScore: "沙盘模拟",
+        teachingScore: "课堂教学"
+      };
+      for (const key in names) {
+        if (names.hasOwnProperty(key)) {
+          const element = names[key];
+          let data = [];
+          for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            data.push(item[key]);
+            console.log(data[i] + "=====" + key);
+          }
+          this.statisticOption.series.push({
+            name: element,
+            data: data
+          });
         }
       }
     },
@@ -313,7 +341,6 @@ export default {
     },
     async list() {
       let res = await axios.$post("workload/list", this.query);
-      console.log(typeof res);
       for (let i = 0; i < res.records.length; i++) {
         res.records[i].scores = 0;
         res.records[i].year = this.query.year;
@@ -322,14 +349,12 @@ export default {
             const element = res.records[i].score[key];
             res.records[i].scores += element[this.query.year];
             res.records[i][key] = res.records[i].score[key][this.query.year];
-            console.log(res.records[i][key] + "======" + key);
           }
         }
         res.records[i].scores = res.records[i].scores.toFixed(2);
       }
       this.total = parseInt(res.total);
       this.tableData = res.records;
-      console.log(this.tableData);
       this.loading = false;
     }
   }
